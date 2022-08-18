0657be73d2f0ce683d1895bac022c98848821f19
MrDevBot <alexeldera04@gmail.com>
2022-08-18 20:26:16 +1000

    Create Language Model Struct

---
+++ PromisedLandDSPBot/modules/Language/Module.cs
@@ -0,0 +1,12 @@
+﻿using DSharpPlus.SlashCommands;
+
+namespace PromisedLandDSPBot.Modules.Language;
+
+public class Module
+{
+    [SlashCommandGroup("gpt", "Language Model Access, Requires GPT-3 Auth")]
+    public class Slash : ApplicationCommandModule
+    {
+        
+    }
+}

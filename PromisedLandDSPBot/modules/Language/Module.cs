b7ed4ba99ce173a422bb43bfc4d95c8b316aa4b4
MrDevBot <alexeldera04@gmail.com>
2022-10-23 20:08:22 +1100

    Disabled GPT3 Module

--- PromisedLandDSPBot/modules/Language/Module.cs
+++ PromisedLandDSPBot/modules/Language/Module.cs
@@ -14,16 +14,16 @@ public class Module
     [SlashCommandGroup("gpt", "Language Model Module"), RequireUserPermissions(Permissions.Administrator), RequireGuild()]
     public class Slash : ApplicationCommandModule
     {
-        OpenAIAPI api = new OpenAIAPI(apiKeys: "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", engine: Engine.Davinci);
+        OpenAIAPI api = new OpenAIAPI(apiKeys: "", engine: Engine.Davinci);
         
         [SlashCommand("request", "repeats the provided phrase")]
         [RequireGuild]
         [RequirePermissions(Permissions.SendMessages)]
-        public async Task Say(InteractionContext ctx,
+        public async Task Request(InteractionContext ctx,
             [Option("prompt", "the message you would like the AI to respond to")] string prompt, [Option("tokens", "how many tokens would you like to allocate to your request")] long tokens)
         {
-            await ctx.CreateResponseAsync("Please Wait...", true);
-            
+            await ctx.CreateResponseAsync("sorry, this module has been disabled.", true);
+            /*
             if (tokens > 100 && !Developer.IsTeamMember(ctx).Result)
             {
                 await ctx.CreateResponseAsync("sorry, but only app developers and owners are allowed to use more than 100 tickets in a single request.", true);
@@ -41,6 +41,7 @@ public class Module
             //await ctx.DeleteResponseAsync();
 
             await ctx.Channel.SendMessageAsync($"{ctx.Member.Mention} asked `{prompt.Replace("`", string.Empty)}`" + result);
+            */
         }
     }
 }
